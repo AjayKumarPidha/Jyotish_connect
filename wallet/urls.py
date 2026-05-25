54f4c5a4f05d9b94ac853e487a2e74b24a876764
AjayKumarPidha <ajaypidha408@gmail.com>
2026-05-25 23:01:07 +0530

    added two files in jyotish project

--- wallet/urls.py
+++ wallet/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('webhook/',        WebhookAPIView.as_view(),        name='razorpay-webhook'),
     path('transactions/',   TransactionListAPIView.as_view(),name='wallet-transactions'),
     path('payout/',         PayoutRequestAPIView.as_view(),  name='wallet-payout'),
-    path('api/wallet/debug-razorpay/', DebugRazorpayAPIView.as_view(), name='debug-razorpay'),
+    path('debug-razorpay/', DebugRazorpayAPIView.as_view(), name='debug-razorpay'),
 ]
